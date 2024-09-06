dff2e16b7a41098123a57c2923d892301fe493a1
Pawel <szpght@gmail.com>
2024-09-06 03:33:49 +0200

    usunięcie domyślnej wartości IsLocal

--- BlazorApp1/ReverseProxy/Component.cs
+++ BlazorApp1/ReverseProxy/Component.cs
@@ -6,6 +6,6 @@ public record Component(
     [property: JsonPropertyOrder(1)] string Name,
     [property: JsonPropertyOrder(2)] string LocalTarget,
     [property: JsonPropertyOrder(3)] string RemoteTarget,
-    [property: JsonPropertyOrder(4)] bool IsLocal = false) {
+    [property: JsonPropertyOrder(4)] bool IsLocal) {
     [JsonIgnore] public string Target => IsLocal ? LocalTarget : RemoteTarget;
 };
